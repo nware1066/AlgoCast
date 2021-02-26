1ccae5d1b3d8445dfa6a6c445b886c9f7d8bf4b4
Naomi Ware <nware1066@yahoo.com>
2021-02-26 12:36:05 -0700

    Add final step to maxChar exercise iterating over charactar map to find solution

--- exercises/maxChar.js
+++ exercises/maxChar.js
@@ -7,17 +7,30 @@
 
 function maxChar(string) {
 
-  const result = {}
+  const charMap = {};
+  let max = 0;
+  let maxChar = '';
 
   for (let char of string) {
-    if (!result[char]) {
-      result[char] = 1;
+    if (!charMap[char]) {
+      charMap[char] = 1;
     } else {
-      result[char]++
+      charMap[char]++
     }
   }
-  return result;
+
+  // use for in loop when iterating over contents of an object
+
+  for ( let char in charMap ) {
+    if (charMap[char] > max ) {
+      max = charMap[char];
+      maxChar = char;
+    }
+  }
+  console.log(maxChar);
+  return maxChar;
 }
 
 
 module.exports = maxChar;
+
