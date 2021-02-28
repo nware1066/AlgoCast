ec7af1f20860fc6bf2e45842484e79d50e8d6008
Naomi Ware <nware1066@yahoo.com>
2021-02-27 18:12:09 -0700

    Solve anagram challenge using charactar map and regex

---
+++ exercises/anagram.js
@@ -0,0 +1,37 @@
+// --- Directions
+// Check to see if two provided strings are anagrams of eachother.
+// One string is an anagram of another if it uses the same characters
+// in the same quantity. Only consider characters, not spaces
+// or punctuation.  Consider capital letters to be the same as lower case
+// --- Examples
+//   anagrams('rail safety', 'fairy tales') --> True
+//   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
+//   anagrams('Hi there', 'Bye there') --> False
+
+function anagrams(stringA, stringB) {
+    // use regex to eleminate spaces and symbols
+    // use toLowerCase to eliminate case sensitivity issues
+    // create character map of each string, if the two maps match exactly, they are anagrams
+    const charMapA = buildCharMap(stringA);
+    const charMapB = buildCharMap(stringB);
+
+    if ( Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+        return false;
+        } 
+            for ( let char in charMapA ) {
+                if (charMapA[char] !== charMapB[char]) {
+                    return false;
+                }
+            }
+        return true;
+    }
+
+function buildCharMap(string) {
+    // create helper function to build character map to avoid redundant code
+    const charMap = {};
+
+    for ( let char of string.replace(/[^\w]/g, '').toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+    return charMap;
+}
