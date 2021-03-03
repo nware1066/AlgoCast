db00fe049459b7ac721d9850e9a9d9627a94658a
Naomi Ware <nware1066@yahoo.com>
2021-03-03 12:49:08 -0700

    Add recursive solution to pyramid challenge

--- exercises/pyramid.js
+++ exercises/pyramid.js
@@ -14,19 +14,44 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-    const mid = Math.floor((2 * n -1) / 2);
+// function pyramid(n) {
+//     const mid = Math.floor((2 * n -1) / 2);
 
-    for ( let row = 0; row < n; row++) {
-        let level = '';
+//     for ( let row = 0; row < n; row++) {
+//         let level = '';
 
-        for ( let column = 0; column < 2 * n -1; column++) {
-            if ( mid - row <= column && mid + row >= column) {
-                level += '#';
-            } else {
-                level += ' ';
-            }
-        }
-        console.log(level);
+//         for ( let column = 0; column < 2 * n -1; column++) {
+//             if ( mid - row <= column && mid + row >= column) {
+//                 level += '#';
+//             } else {
+//                 level += ' ';
+//             }
+//         }
+//         console.log(level);
+//     }
+// }
+
+
+// base case = row = n;
+// each recursion, row increases by 1;
+// default row = 0, level = ''
+
+function pyramid(n, row = 0, level = '') {
+    if ( row === n ) {
+        return;
     }
+
+    if ( level.length === 2 * n -1 ) {
+        console.log(level);
+        return pyramid(n, row + 1)
+    }
+
+    const mid = Math.floor((2 * n -1) /2);
+    let add;
+    if (mid - row <= level.length && mid + row >= level.length) {
+        add = '#';
+    } else {
+        add = ' ';
+    }
+    pyramid(n, row, level + add);
 }
