8009db5e67bf58a7cf19c1ad0331c07855cc449e
Naomi Ware <nware1066@yahoo.com>
2021-03-06 21:15:44 -0700

    Add iterative solution to vowels challenge

---
+++ exercises/vowels.js
@@ -0,0 +1,19 @@
+// --- Directions
+// Write a function that returns the number of vowels
+// used in a string.  Vowels are the characters 'a', 'e'
+// 'i', 'o', and 'u'.
+// --- Examples
+//   vowels('Hi There!') --> 3
+//   vowels('Why do you ask?') --> 4
+//   vowels('Why?') --> 0
+
+function vowels(str) {
+    let result = 0;
+    const target = ['a', 'e', 'i', 'o', 'u'];
+    for (let char of str.toLowerCase()) {
+        if (target.includes(char)) {
+            result++;
+        }
+    }
+    return result;
+}
