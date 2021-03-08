4138daac705605aa0b1ebd530b9591cd69894e1e
Naomi Ware <nware1066@yahoo.com>
2021-03-08 15:26:35 -0700

    Add recursive solution to fibonacci challenge

--- exercises/fib.js
+++ exercises/fib.js
@@ -8,19 +8,31 @@
 // Example:
 //   fib(4) === 3
 
+// function fib(n) {
+//     // create a new variable with the value of an array containing elements 0 and 1
+//     // if the length of the array is less than n + 1 ( because the first 2 numbers will have to be added together prior to the sequence starting) keep going ( this could be recursive, maybe)
+//     // each iteration will take the last 2 elements and add them, pushing the solution into the new array
+//     // return the solution at the index equal to the provided argument ( or the last index of the array (array.length - 1))
+//     const sequence = [0, 1];
+
+//     for (i = 2; i <= n; i++) {
+//         const a = sequence[i - 1];
+//         const b = sequence[i - 2];
+
+//         sequence.push(a + b);
+//     }
+
+//     return sequence[sequence.length -1];
+// }
+
+
+// pseudo-code
+// this is a challenge that pretty much relies on memorization. It's kind of ridiculous to wrap your head around.
+// the code is finding out how many times you have to add the number - 1 and the number - 2 together before you hit the provided number and then returning the number of iterations. YIKES the runtime for this is dreadful O(2 ofnsquared) Quadratic runtime.
+
 function fib(n) {
-    // create a new variable with the value of an array containing elements 0 and 1
-    // if the length of the array is less than n + 1 ( because the first 2 numbers will have to be added together prior to the sequence starting) keep going ( this could be recursive, maybe)
-    // each iteration will take the last 2 elements and add them, pushing the solution into the new array
-    // return the solution at the index equal to the provided argument ( or the last index of the array (array.length - 1))
-    const sequence = [0, 1];
-
-    for (i = 2; i <= n; i++) {
-        const a = sequence[i - 1];
-        const b = sequence[i - 2];
-
-        sequence.push(a + b);
-    }
-
-    return sequence[sequence.length -1];
+    if (n < 2) {
+        return n;
+    } 
+    return fib(n - 1) + fib(n - 2)
 }
