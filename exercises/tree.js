16f425248c0d0cf38441915a1a80e2abb047070a
Naomi Ware <nware1066@yahoo.com>
2021-03-15 22:13:39 -0600

    Add function for breadth first traversal of tree

--- exercises/tree.js
+++ exercises/tree.js
@@ -34,6 +34,16 @@ class Tree {
         this.root = null;
     }
 
+    traverseBF(fn) {
+        const arr = [this.root];
+        while (arr.length) {
+            const node = arr.shift();
+        
+            arr.push(...node.children);
+            fn(node);
+        }
+
+    }
  
 }
 
